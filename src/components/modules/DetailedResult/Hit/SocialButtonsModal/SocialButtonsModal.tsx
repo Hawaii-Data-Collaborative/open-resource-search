9efb545131926cb57f7518a085f23a0c43f8312f
Kyle Mulleady <kyle@windwardapps.com>
2022-12-13 17:24:54 -1000

    print/share analytics

--- src/components/modules/DetailedResult/Hit/SocialButtonsModal/SocialButtonsModal.tsx
+++ src/components/modules/DetailedResult/Hit/SocialButtonsModal/SocialButtonsModal.tsx
@@ -1,6 +1,7 @@
 import Text from 'src/components/elements/Text/Text';
 import Modal from 'src/components/elements/Modal/Modal';
 import Flex from 'src/components/elements/Flex/Flex';
+import { logEvent } from 'src/analytics';
 
 import {
   PrintButton,
@@ -12,6 +13,37 @@ import {
 export default function SocialButtonsModal({ share, setShare, hit }) {
   if (!hit) return null;
 
+  const onPrintButtonClick = () => {
+    logEvent('Referral.Print', {
+      currentPage: window.location.toString(),
+      program: hit.title,
+    });
+    if (process.browser) {
+      window.print();
+    }
+  };
+
+  const onEmailButtonClick = () => {
+    logEvent('Referral.Share.Email', {
+      currentPage: window.location.toString(),
+      program: hit.title,
+    });
+  };
+
+  const onFBButtonClick = () => {
+    logEvent('Referral.Share.Facebook', {
+      currentPage: window.location.toString(),
+      program: hit.title,
+    });
+  };
+
+  const onTwitterButtonClick = () => {
+    logEvent('Referral.Share.Twitter', {
+      currentPage: window.location.toString(),
+      program: hit.title,
+    });
+  };
+
   return (
     <Modal open={share} handleClose={() => setShare(false)}>
       <Text variant="h2" color="primary" textAlign="center" marginBottom="24px">
@@ -19,7 +51,7 @@ export default function SocialButtonsModal({ share, setShare, hit }) {
       </Text>
 
       <Flex flexDirection="column">
-        <PrintButton onClick={process.browser ? window.print : null}>
+        <PrintButton onClick={onPrintButtonClick}>
           Print this Listing
         </PrintButton>
 
@@ -27,6 +59,7 @@ export default function SocialButtonsModal({ share, setShare, hit }) {
           href={`mailto:?subject=${hit.title}&body=${
             process.browser ? window.location.href : ''
           }`}
+          onClick={onEmailButtonClick}
         >
           Share with Email
         </EmailButton>
@@ -37,6 +70,7 @@ export default function SocialButtonsModal({ share, setShare, hit }) {
           href={`https://www.facebook.com/sharer/sharer.php?u=${
             process.browser ? window.location.href : ''
           }`}
+          onClick={onFBButtonClick}
         >
           Share on Facebook
         </FacebookButton>
@@ -47,6 +81,7 @@ export default function SocialButtonsModal({ share, setShare, hit }) {
           href={`https://twitter.com/share?text=${hit.title}&url=${
             process.browser ? window.location.href : ''
           }`}
+          onClick={onTwitterButtonClick}
         >
           Share on Twitter
         </TwitterButton>
