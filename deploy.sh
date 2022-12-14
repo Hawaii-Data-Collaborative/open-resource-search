7f1554a6ade2a6d93752e13aa4b6651547c43b67
Kyle Mulleady <kyle@windwardapps.com>
2022-12-13 14:58:30 -1000

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #! /usr/bin/env bash
 
+echo "[deploy] pushing ..."
+git push beta main
+
 echo "[deploy] scp'ing ..."
 scp next.tar.gz wwa:/var/www/auwsearch.windwardapps.com/open-resource-search/
 
 echo "[deploy] ssh'ing ..."
 ssh wwa bash << EOF
 cd /var/www/auwsearch.windwardapps.com/open-resource-search
+git reset --hard
 if [ -d "./.next" ]; then
   echo "[deploy] moving existing .next dir to .next-old ..."
   rm -rf .next-old
