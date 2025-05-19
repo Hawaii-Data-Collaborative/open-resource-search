4c1e044fd9eb7db04db3b45ccca3345466a89c05
Kyle Mulleady <kmulleady@hawaiidata.org>
2025-05-18 17:28:37 -1000

    build.sh

--- build.sh
+++ build.sh
@@ -17,13 +17,27 @@ fi
 echo "[deploy] configFile=$configFile"
 
 echo "[build] collecting configs ..."
+mv .env .env.development
+mv .env.production .env
 mv app.config.json app.config.dev.json
 mv $configFile app.config.json
+
+if [[ -d "dist" ]]; then
+  if [[ -d "dist-old" ]]; then
+    echo "[build] removing old 'dist-old' directory ..."
+    rm -rf dist-old
+  fi
+
+  echo "[build] 'dist' directory exists, moving to 'dist-old' ..."
+  mv dist dist-old
+fi
 
 echo "[build] building ..."
 $buildCmd
 
 mv app.config.json $configFile
 mv app.config.dev.json app.config.json
+mv .env .env.production
+mv .env.development .env
 
 echo "[build] done"
